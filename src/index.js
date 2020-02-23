5a26d6f89cfb12075c3031d388c768a9526cd9cc
danielmitiku <wedanielmitiku@gmail.com>
2020-02-23 22:30:47 +0300

    added createStore to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>, document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, 
+  document.getElementById('root'));
 
 serviceWorker.register();
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+
+const initialState = {
+
+};
+
+  const store = createStore( rootReducer, initialState );
+
+  export default store;
