93050f5616d07ae6af747c44d9d4f6b657997bc1
danielmitiku <wedanielmitiku@gmail.com>
2020-02-23 21:58:00 +0300

    added login form with client side validation

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,74 @@
-import React from 'react'
+import React from 'react';
 
-const Login = () => {
-  return (
-    <div className="main">
-      <h1>Login</h1>
-    </div>
-  );
-}
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.initialState = {
+      email: '',
+      password: '',
+      errors: {},
+    };
+    this.state = { ...this.initialState };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.validateForm = this.validateForm.bind(this);
+  }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    if( this.validateForm() === true ) {
+      console.log('form submitted')
+      //fire_action
+      //this.setState({ ...this.initialState });
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  validateForm = () => {
+    const { email, password } = this.state;
+    let errors = {};
+    if (!email) {
+      errors.email = 'This field is required';
+    }
+    if (!password) {
+      errors.password = 'This field is required';
+    }
+    this.setState({errors});
+    if (email && password) {
+      return true;
+    }
+    return false;
+  }
+
+  render() {
+    const { email, password, errors } = this.state;
+    return (
+      <div className="col-md-4 mt-4 offset-4">
+        <h2 className="main">Login</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Email address</label>
+            <input type="email" className={`form-control ${errors.email ? "is-invalid" : ""}`} id="email" name="email" value={email} onChange={this.handleChange}/>
+            {errors.email && <span className={`${errors.email ? "invalid-feedback" : ""}`}>{errors.email}</span>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" className={`form-control ${errors.password ? "is-invalid" : ""}`} id="password" name="password" value={password} onChange={this.handleChange}/>
+            {errors.password && <span className={`${errors.password ? "invalid-feedback" : ""}`}>{errors.password}</span>}
+          </div>
+          <div className="main">
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+          );
+        }
+        
+        }
+           
 export default Login;
--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,11 +1,70 @@
 import React from 'react'
 
-const SignUp = () => {
-  return (
-    <div className="main">
-      <h1>Signup</h1>
-    </div>
-  );
-}
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.initialState = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      password_confirmation: ''
+    };
+    this.state = { ...this.initialState };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    const { firstName, lastName, email, password, password_confirmation } = this.state;
+    if( firstName && lastName && email && password && password_confirmation ) {
+      //fire_action
+      this.setState({ ...this.initialState });
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  render() {
+    const { firstName, lastName, email, password, password_confirmation } = this.state;
+    return (
+      <div className="col-md-4 mt-4 offset-4">
+        <h2 className="main">Sign Up</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+              <label for="firstName">First Name</label>
+              <input type="text" className="form-control" id="firstName" name="firstName" value={firstName} onChange={this.handleChange}/>
+          </div>
+          <div className="form-group">
+              <label for="lastName">Last Name</label>
+              <input type="text" className="form-control" id="lastName" name="lastName" value={lastName} onChange={this.handleChange}/>
+          </div>
+          <div className="form-group">
+            <label for="email">Email address</label>
+            <input type="email" className="form-control" id="email" name="email" value={email} onChange={this.handleChange}/>
+              <small id="emailHelp" className="form-text text-muted">Your email is safe in our hands.</small>
+          </div>
+          <div className="form-group">
+            <label for="password">Password</label>
+            <input type="password" className="form-control" id="password" name="password" value={password} onChange={this.handleChange}/>
+          </div>
+          <div className="form-group">
+            <label for="passwordConfirmation">Password Confirmation</label>
+            <input type="password" className="form-control" id="password_confirmation" name="password_confirmation" value={password_confirmation} onChange={this.handleChange}/>
+          </div>
+          <div class="main">
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+        
+}
+           
 export default SignUp;
