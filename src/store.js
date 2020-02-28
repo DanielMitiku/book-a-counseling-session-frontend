f5c55b0f3501091d78c7645ffbf84e71c1a017f8
danielmitiku <wedanielmitiku@gmail.com>
2020-02-28 17:02:09 +0300

    added jwtToken to be checked from localStorage

--- src/store.js
+++ src/store.js
@@ -1,11 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
+import setAuthorizationToken from './utils/auth_header';
+import { setCurrentUserId } from './actions/authAction';
+import jwtDecode from 'jwt-decode';
 
 const initialState = {
 
 };
 
   const store = createStore( rootReducer, initialState, applyMiddleware(thunk) );
+
+  if (localStorage.jwtToken) {
+    setAuthorizationToken(localStorage.jwtToken);
+    store.dispatch(setCurrentUserId(jwtDecode(localStorage.jwtToken)));
+  }
 
   export default store;
