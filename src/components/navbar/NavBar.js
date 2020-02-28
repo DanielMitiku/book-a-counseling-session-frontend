e1ac48e6a2e05094aea8c1c44a2764093eb2e87b
danielmitiku <wedanielmitiku@gmail.com>
2020-02-28 17:00:34 +0300

    updated Navbar.js

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/authAction';
 
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const { isAuthenticated, user, loggingIn } = props.auth;
+  const logoutHandler = (event) => {
+    event.preventDefault();
+    props.logout();
+  }
+  const loginLink = (
+      <ul className="nav">
+        <li className="nav-item align-right">
+          <Link to='/login' className="nav-link nav-color">Login</Link>
+        </li>
+        <li className="nav-item align-right">
+          <Link to='signup' className="nav-link nav-color">Sign Up</Link>
+        </li>
+      </ul>
+  );
+  const logoutLink = (
+      <ul className="nav">
+        <li className="nav-item align-right">
+          <Link to='/logout' onClick={logoutHandler} className="nav-link nav-color">Logout</Link>
+        </li>
+      </ul>
+  );
   return (
     <nav className="navbar fixed-top navbar-dark bg-dark">
       <li className="nav-item">
@@ -19,17 +43,16 @@ const NavBar = () => {
           <Link to='/' className="nav-link nav-color">Link</Link>
         </li>
       </ul>
-      <ul className="nav">
-        <li className="nav-item align-right">
-          <Link to='/login' className="nav-link nav-color">Login</Link>
-        </li>
-        <li className="nav-item align-right">
-          <Link to='signup' className="nav-link nav-color">Sign Up</Link>
-        </li>
-      </ul>
+      { isAuthenticated ? logoutLink : loginLink }      
     </nav>
 
   )
 };
 
-export default NavBar;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  }
+}
+
+export default connect(mapStateToProps, { logout })(NavBar);
