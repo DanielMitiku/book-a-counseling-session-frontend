eca7f396a25c3ea501ba515fd8cc13461b32ec61
danielmitiku <wedanielmitiku@gmail.com>
2020-02-28 06:07:49 +0300

    updated signup actions

--- src/actions/signupAction.js
+++ src/actions/signupAction.js
@@ -1,7 +1,9 @@
 import { config } from '../utils/config';
 import axios from 'axios';
-import { SIGNUP_REQUEST, SIGNUP_SUCCESS, SIGNUP_FAILURE } from './types';
+import { SIGNUP_REQUEST, SIGNUP_SUCCESS, SIGNUP_FAILURE, SET_CURRENT_USER_ID } from './types';
 import { alert_success, alert_error } from './alertAction';
+import jwtDecode from 'jwt-decode';
+import setAuthorizationToken from '../utils/auth_header'
 
 const signupUser = (user, history) => {
   return dispatch => {
@@ -9,6 +11,10 @@ const signupUser = (user, history) => {
     return axios.post(`${config.url.BASE_URL}/signup`, user)
     .then((response) => {
       dispatch(signupSuccess(response));
+      const token = response.data.auth_token;
+      localStorage.setItem('jwtToken', token);
+      setAuthorizationToken(token);
+      dispatch(setCurrentUserId(jwtDecode(token).user_id));
       dispatch(alert_success('Signup successful!'));
       history.push('/');
     })
@@ -37,6 +43,13 @@ const signupFailure = (error) => {
   return {
     type: SIGNUP_FAILURE,
     error
+  }
+}
+
+const setCurrentUserId = (user_id) => {
+  return {
+    type: SET_CURRENT_USER_ID,
+    user_id
   }
 }
 
