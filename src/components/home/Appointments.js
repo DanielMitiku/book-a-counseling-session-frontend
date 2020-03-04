6b481281c3c1166ff0d8938a521086dbfb0f1289
danielmitiku <wedanielmitiku@gmail.com>
2020-03-05 01:52:31 +0300

    updated appointment component

--- src/components/home/Appointments.js
+++ src/components/home/Appointments.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getAppointments } from '../../actions/appointmentAction';
+import { getAppointments, deleteAppointment } from '../../actions/appointmentAction';
+import jwtDecode from 'jwt-decode';
+
 
 class Appointments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.deleteHandler = this.deleteHandler.bind(this);
+  }
+
   componentDidMount() {
-    const  { user_id } = this.props.auth;
+    const user_id  = jwtDecode(localStorage.jwtToken).user_id;
     const { getAppointments } = this.props;
     getAppointments(user_id);
   }
 
+  deleteHandler = (appointment_id) => {
+    const user_id  = jwtDecode(localStorage.jwtToken).user_id;
+    const { deleteAppointment } = this.props;
+    deleteAppointment(appointment_id, user_id);
+  }
+
   render() {
     const { appointment } = this.props;
+    const loading = (<div class="d-flex justify-content-center">
+                      <div className="spinner-border text-primary" role="status">
+                       <span className="sr-only">Loading...</span>
+                      </div>
+                    </div>);
     return (
       <div className='main-bar bar'>
         <h3 className='text-center'>Here are your Appointments</h3>
+        {appointment.requesting && loading}
         <div className='row'>
         { appointment.appointments && appointment.appointments.map(a => {
           return (
-            <div key={a.id} className="card col-md-5 text-center mx-4 my-4" style={{width: '25rem'}}>
+            <div key={a.appointment_id} className="card col-md-5 text-center mx-4 my-4" style={{width: '25rem'}}>
               <div className="card-body">
-                <h5 className="card-title">{a.date}</h5>
-                <p className="card-text">{a.date}</p>
+                <h5 className="card-title">{a.appointment_date}</h5>
+                <p className="card-text">Counseling Title: {a.counseling_name}</p>
+                <p className="card-text">Description: {a.counseling_desc}</p>
+                <button type="button" className="btn btn-sm btn-danger" onClick={() => {this.deleteHandler(a.appointment_id)}}>Cancel Appointment</button>
               </div>
             </div>
           );
@@ -35,8 +56,7 @@ class Appointments extends React.Component {
 const mapStateToProps = (state) => {
   return {
     appointment: state.appointment,
-    auth: state.auth,
   }
 }
 
-export default connect(mapStateToProps, { getAppointments })(Appointments);
+export default connect(mapStateToProps, { getAppointments, deleteAppointment })(Appointments);
