0a6ef4d5dc4f2fde812d520d87e96c0bd6a6d934
danielmitiku <wedanielmitiku@gmail.com>
2020-03-05 01:51:01 +0300

    fixed minor bug while checking jwtToken

--- src/store.js
+++ src/store.js
@@ -2,10 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import setAuthorizationToken from './utils/auth_header';
-import { setCurrentUserId } from './actions/authAction';
 import jwtDecode from 'jwt-decode';
 import history from "./utils/history";
-import { loginSuccess, loginFailure } from './actions/authAction';
+import { loginSuccess, loginFailure, setCurrentUserId, loginRequest } from './actions/authAction';
 import axios from 'axios';
 import { config } from './utils/config';
 
@@ -16,15 +15,17 @@ const initialState = {
   const store = createStore( rootReducer, initialState, applyMiddleware(thunk) );
 
   if (localStorage.jwtToken) {
+    store.dispatch(loginRequest());
     const user_id = jwtDecode(localStorage.jwtToken).user_id;
+    setAuthorizationToken(localStorage.jwtToken);
     axios.get(`${config.url.BASE_URL}/users/${user_id}`)
     .then(() => {
-      setAuthorizationToken(localStorage.jwtToken);
       store.dispatch(setCurrentUserId(user_id));
       store.dispatch(loginSuccess(user_id));
     })
     .catch((error) => {
       store.dispatch(loginFailure(error));
+      setAuthorizationToken(false);
       history.push('/login');
     });
   }
