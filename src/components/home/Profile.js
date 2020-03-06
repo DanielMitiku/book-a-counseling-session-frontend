868dc5cb2edcac90e8596e7dcf2fe743646b30f4
danielmitiku <wedanielmitiku@gmail.com>
2020-03-07 00:03:59 +0300

    implemented profile component

--- src/components/home/Profile.js
+++ src/components/home/Profile.js
@@ -1,14 +1,43 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { getUser } from '../../actions/userAction';
+import jwtDecode from 'jwt-decode';
 
 class Profile extends React.Component {
+  componentDidMount() {
+    const user_id = jwtDecode(localStorage.jwtToken).user_id;
+    const { getUser } = this.props;
+    getUser(user_id);
+  }
+
   render() {
+    const { user } = this.props;
+    const loading = (<div className="d-flex justify-content-center">
+                      <div className="spinner-border text-primary" role="status">
+                       <span className="sr-only">Loading...</span>
+                      </div>
+                    </div>);
     return (
       <div className='main-bar bar'>
-        <p>Profile Bar</p>
+        <h3 className='text-center'>Your Profile</h3>
+        { user.requesting && loading}
+        <div className='col-md-4 offset-3'>
+          <div className="card text-center mx-4 my-4" style={{width: '25rem'}}>
+            <div className="card-body">
+              <h5 className="card-title">{user.user.first_name} {user.user.last_name}</h5>
+              <p className="card-text">{user.user.email}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Profile;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  }
+}
+
+export default connect(mapStateToProps, { getUser })(Profile);
