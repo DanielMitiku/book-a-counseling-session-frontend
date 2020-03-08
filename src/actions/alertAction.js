e683ad435e33811ca6a2e64601b3b148a70eaa32
danielmitiku <wedanielmitiku@gmail.com>
2020-03-08 22:34:16 +0300

    added alert timeout for flash messages

--- src/actions/alertAction.js
+++ src/actions/alertAction.js
@@ -1,24 +1,18 @@
-  import { ALERT_ERROR, ALERT_SUCCESS, ALERT_CLEAR } from './types';
+import { ALERT_ERROR, ALERT_SUCCESS, ALERT_CLEAR } from './types';
 
 
-  const alert_success = (payload) => {
-    return {
-      type: ALERT_SUCCESS,
-      payload
-    }
-  }
+const alert_success = payload => ({
+  type: ALERT_SUCCESS,
+  payload,
+});
 
-  const alert_error = (payload) => {
-    return {
-      type: ALERT_ERROR,
-      payload
-    }
-  }
+const alert_error = payload => ({
+  type: ALERT_ERROR,
+  payload,
+});
 
-  const alert_clear = () => {
-    return {
-      type: ALERT_CLEAR,
-    }
-  }
+const alert_clear = () => ({
+  type: ALERT_CLEAR,
+});
 
-  export { alert_error, alert_success, alert_clear };
+export { alert_error, alert_success, alert_clear };
