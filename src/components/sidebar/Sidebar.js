5a5f69df4eb55f51c88c473dce29880e5bde9add
danielmitiku <wedanielmitiku@gmail.com>
2020-04-15 14:40:36 +0300

    update sidebar style

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -16,31 +16,31 @@ const Sidebar = props => {
 
   const loginLink = (
     <ul className="nav flex-column">
-      <Link to="/login" className={`side-bar-item py-3 px-5 ${currentUrl === '/login' ? 'side-bar-active' : ''}`}>Login</Link>
-      <Link to="/signup" className={`side-bar-item py-3 px-5 ${currentUrl === '/signup' ? 'side-bar-active' : ''}`}>Sign Up</Link>
+      <Link to="/login" className={`side-bar-item py-3  ${currentUrl === '/login' ? 'side-bar-active' : ''}`}>Login</Link>
+      <Link to="/signup" className={`side-bar-item py-3 ${currentUrl === '/signup' ? 'side-bar-active' : ''}`}>Sign Up</Link>
     </ul>
   );
 
   const userLink = (
     <ul className="nav flex-column">
-      <Link to="/sessions" className={`side-bar-item py-3 px-5 ${currentUrl.includes('/sessions') ? 'side-bar-active' : ''}`}>Book Now</Link>
-      <Link to="/appointments" className={`side-bar-item py-3 px-5 ${currentUrl === '/appointments' ? 'side-bar-active' : ''}`}>Appointments</Link>
-      <Link to="/profile" className={`side-bar-item py-3 px-5 ${currentUrl === '/profile' ? 'side-bar-active' : ''}`}>Profile</Link>
-      <Link to="/logout" onClick={logoutHandler} className={`side-bar-item py-3 px-5 ${currentUrl === '/logout' ? 'side-bar-active' : ''}`}>Logout</Link>
+      <Link to="/sessions" className={`side-bar-item py-3 ${currentUrl.includes('/sessions') ? 'side-bar-active' : ''}`}>Book Now</Link>
+      <Link to="/appointments" className={`side-bar-item py-3 ${currentUrl === '/appointments' ? 'side-bar-active' : ''}`}>Appointments</Link>
+      <Link to="/profile" className={`side-bar-item py-3 ${currentUrl === '/profile' ? 'side-bar-active' : ''}`}>Profile</Link>
+      <Link to="/logout" onClick={logoutHandler} className={`side-bar-item py-3 ${currentUrl === '/logout' ? 'side-bar-active' : ''}`}>Logout</Link>
     </ul>
   );
 
   const adminLink = (
     <ul className="nav flex-column">
-      <Link to="/users" className={`side-bar-item py-3 px-5 ${currentUrl === '/users' ? 'side-bar-active' : ''}`}>
+      <Link to="/users" className={`side-bar-item py-3 ${currentUrl === '/users' ? 'side-bar-active' : ''}`}>
         Users
         <small className="text-muted" style={{ fontSize: '10px' }}> admin</small>
       </Link>
-      <Link to="/all_appointments" className={`side-bar-item py-3 px-5 ${currentUrl === '/all_appointments' ? 'side-bar-active' : ''}`}>
+      <Link to="/all_appointments" className={`side-bar-item py-3 ${currentUrl === '/all_appointments' ? 'side-bar-active' : ''}`}>
         All Booked
         <small className="text-muted" style={{ fontSize: '10px' }}> admin</small>
       </Link>
-      <Link to="/counselings" className={`side-bar-item py-3 px-5 ${currentUrl === '/counselings' ? 'side-bar-active' : ''}`}>
+      <Link to="/counselings" className={`side-bar-item py-3 ${currentUrl === '/counselings' ? 'side-bar-active' : ''}`}>
         Counselings
         <small className="text-muted" style={{ fontSize: '10px' }}> admin</small>
       </Link>
@@ -49,9 +49,9 @@ const Sidebar = props => {
 
   return (
     <div className="side-bar">
-      <div className="side-bar-body text-left">
+      <div className="side-bar-body text-center">
         <ul className="nav flex-column">
-          <Link to="/" className={`side-bar-item py-3 px-5 ${currentUrl === '/' ? 'side-bar-active' : ''}`}>Home</Link>
+          <Link to="/" className={`side-bar-item py-3 ${currentUrl === '/' ? 'side-bar-active' : ''}`}>Home</Link>
         </ul>
         { is_privileged ? adminLink : '' }
         { isAuthenticated ? userLink : loginLink }
